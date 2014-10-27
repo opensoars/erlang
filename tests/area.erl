d10862a3db634e6ad53ee1e90bdee3cede57489a
opensoars <sam_1700@hotmail.com>
2014-10-27 20:29:26 +0100

    area working

--- tests/area.erl
+++ tests/area.erl
@@ -1,5 +1,17 @@
 -module(area).
 -export([calc/1]).
 
+% calc: calculates area of shapes
+
 calc({square, Side}) ->
-  Side * Side.
+  Side * Side;
+
+calc({circle, Radius}) ->
+  math:pi() * Radius * Radius;
+
+calc({triangle, A, B, C}) ->
+  S = (A + B + C)/2,
+  math:sqrt(S*(S-A)*(S-B)*(S-C));
+
+calc(Other) ->
+  {error, invalid_object}.
