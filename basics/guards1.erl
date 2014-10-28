1442fb1bd1298635a55528d78ebd45b2f25d59e3
opensoars <sam_1700@hotmail.com>
2014-10-28 16:11:17 +0100

    Added wrong type catch to even

--- basics/guards1.erl
+++ basics/guards1.erl
@@ -2,7 +2,8 @@
 -export([even/1, number/1]).
 
 even(Int) when Int rem 2 == 0 -> true;
-even(Int) when Int rem 2 == 1 -> false.
+even(Int) when Int rem 2 == 1 -> false;
+even(_Other)                  -> wrong_type.
 
 number(Num) when is_integer(Num) -> integer;
 number(Num) when is_float(Num)   -> float;
