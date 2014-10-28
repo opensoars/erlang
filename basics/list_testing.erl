0b4da0b93af0ca3e8b8ad102ebc3acb25d1e563e
opensoars <sam_1700@hotmail.com>
2014-10-28 13:11:25 +0100

    Expanded listlen with a case expression

--- basics/list_testing.erl
+++ basics/list_testing.erl
@@ -1,15 +1,24 @@
 -module(list_testing).
--export([listlen/1]).
+-export([listlen/1, listlen_case/1]).
 
 % Returns the length of a list thats given as an argument.
 
 % When we pass in an empty list, simply return 0
-listlen([]) -> 0;
-
+%
 % When we pass in a list containing atleast one element
 % It will be split into head and tail.
 % The return value gets incremented by 1.
 % We then recusively call listlen again with the tail of the list (remainder).
 % So the function gets called and again and the return value gets increased
 % by 1 again
+listlen([]) -> 0;
 listlen([_|Xs]) -> 1 + listlen(Xs).
+
+
+% Doing the same as in the above function but now we use a case expression
+% to keep everything in one function
+listlen_case(Y) ->
+  case Y of
+    [] -> 0;
+    [_|Xs] -> 1 + listlen_case(Xs)
+  end.
