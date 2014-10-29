9328c4865e61315cebe45cf0223f7b24755464b0
opensoars <sam_1700@hotmail.com>
2014-10-29 13:31:19 +0100

    Added tail_recursion:bump/2 using an accumulating parameter

--- basics/tail_recursion.erl
+++ basics/tail_recursion.erl
@@ -1,11 +1,19 @@
 % c(tail_recursion).
 % tail_recursion:
 
+%
+% Tail recursion
+%
+
 -module(tail_recursion).
--export([sum/2]).
+-export([sum/2, bump/2]).
 
 
 % sum, using an accumulating parameter (Sum)
-
 sum([], Sum) -> Sum;
 sum([H | T], Sum) -> sum(T, H + Sum).
+
+
+% bump, using an accumulating parameter (List)
+bump([], List) -> List;
+bump([H | T], List) -> bump(T, [H + 1 | Acc]).
