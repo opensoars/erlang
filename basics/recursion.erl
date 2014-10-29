806ea8567a673c3baab58244c41af2e9684b148f
opensoars <sam_1700@hotmail.com>
2014-10-29 12:07:13 +0100

    recursion:sum completed

--- basics/recursion.erl
+++ basics/recursion.erl
@@ -10,10 +10,11 @@ bump([Head | Tail]) -> [Head + 1 | bump(Tail)].
 len([]) -> 0.
 
 % Returns the average of integer elements in a list
-average([]) -> 0.
+average(List) -> sum(List) / len(List).
 
 % Returns value of adding every integer element in a list
-sum([]) -> 0.
+sum([]) -> 0;
+sum([Head | Tail]) -> Head + sum(Tail).
 
 % Return true if integer parameter is even false if it's not
 even([]) -> 0.
