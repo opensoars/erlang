e7c168deff5e2d5afeb119fb536d999e94f763ba
opensoars <sam_1700@hotmail.com>
2014-10-30 12:27:18 +0100

    Fixed empty list error

--- basics/tail_opt.erl
+++ basics/tail_opt.erl
@@ -8,6 +8,7 @@
 
 % Returns the average value of a list, by calling the accumulator function
 % that takes as parameters: List, Sum, Length
+average([]) -> 0;
 average(List) -> average_acc(List, 0, 0).
 
 average_acc([], Sum, Length) -> Sum / Length;
