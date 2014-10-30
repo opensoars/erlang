e8cf1994b28b8dac1cad8dfcaad96d6933d41ddb
opensoars <sam_1700@hotmail.com>
2014-10-30 12:29:20 +0100

    Small style edit to tail_opt

--- basics/tail_opt.erl
+++ basics/tail_opt.erl
@@ -11,5 +11,7 @@
 average([]) -> 0;
 average(List) -> average_acc(List, 0, 0).
 
-average_acc([], Sum, Length) -> Sum / Length;
-average_acc([H | T], Sum, Length) -> average_acc(T, Sum + H, Length + 1).
+average_acc([], Sum, Length) ->
+  Sum / Length;
+average_acc([H | T], Sum, Length) ->
+  average_acc(T, Sum + H, Length + 1).
