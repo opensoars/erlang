d4a01ab3e3577ebfc6777f937c266f03058e8946
opensoars <sam_1700@hotmail.com>
2014-10-30 15:03:31 +0100

    Completed exercise 3-1 100%! <3 :)

--- basics/ex3_1.erl
+++ basics/ex3_1.erl
@@ -1,8 +1,23 @@
 -module(ex3_1).
--export([sum/1]).
+-export([sum1/1, sum2/2]).
 
-sum(Boundary) -> sum_acc(1, Boundary, 0).
+% Will sum integers from 1 to Boundary
+% ex3_1:sum1(3). == 6
+sum1(Boundary) -> sum1_acc(1, Boundary, 0).
 
-sum_acc(Index, Boundary, Sum) when Index =< Boundary ->
-  sum_acc(Index + 1, Boundary, Sum + Index); 
-sum_acc(_Index, _Boundary, Sum) -> Sum.
+sum1_acc(Index, Boundary, Sum) when Index =< Boundary ->
+  sum1_acc(Index + 1, Boundary, Sum + Index); 
+sum1_acc(_Index, _Boundary, Sum) -> Sum.
+
+% Will sum integers from Index to Boundary
+% ex3_2:sum(1,3). == 6
+% It will throw when we the Index > Boundary: ex3_2:sum(3,1).
+sum2(Index, Boundary) when Index > Boundary ->
+  throw({ error, {"Index > Boundary", Index, Boundary} });
+sum2(Index, Boundary) ->
+  sum2_acc(Index, Boundary, 0).
+
+sum2_acc(Index, Boundary, Sum) when Index =< Boundary ->
+  sum2_acc(Index + 1, Boundary, Sum + Index);
+sum2_acc(_Index, _Boundary, Sum) ->
+  Sum.
