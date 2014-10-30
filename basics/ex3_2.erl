55128875ec2f31851a09c5adad7444523e564bda
opensoars <sam_1700@hotmail.com>
2014-10-30 17:14:42 +0100

    Completed exercise 3-2 100%

--- basics/ex3_2.erl
+++ basics/ex3_2.erl
@@ -1,21 +1,26 @@
 -module(ex3_2).
--export([reverse_create/1]).
+-export([create/1, reverse_create/1]).
 
 %%----------------------------------------------------------------------
-%% Creates a list from 1 to N given as param
+%% Creates a list from 1 to param N
 %% ex3_2:create(3). => [1,2,3]
 %%----------------------------------------------------------------------
+create(N) -> create_acc(N, 1, []).
 
+create_acc(Index, Final, List) when Final =< Index ->
+  create_acc(Index -1, Final, [Index | List]);
+create_acc(_End, _Index, List) ->
+  List.
 
 
 
 %%----------------------------------------------------------------------
-%% Creates a list from N given as param to 1
+%% Creates a list from param N to 1
 %% ex3_2:reverse_create(3). => [3,2,1]
 %%----------------------------------------------------------------------
 reverse_create(N) -> reverse_create_acc(1, N, []).
 
-reverse_create_acc(Index, End, List) when Index =< End ->
-  reverse_create_acc(Index + 1, End, [Index | List]);
+reverse_create_acc(Index, Final, List) when Index =< Final ->
+  reverse_create_acc(Index + 1, Final, [Index | List]);
 reverse_create_acc(_Index, _End, List) ->
   List.
