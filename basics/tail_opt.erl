0e18bdb908d34eed77690ace182be1a1f62848e8
opensoars <sam_1700@hotmail.com>
2014-10-30 12:42:54 +0100

    Added travis yaml

--- basics/tail_opt.erl
+++ basics/tail_opt.erl
@@ -4,6 +4,7 @@
 
 -module(tail_opt).
 -export([average/1]).
+-include_lib("eunit/include/eunit.hrl").
 
 
 % Returns the average value of a list, by calling the accumulator function
@@ -15,3 +16,6 @@ average_acc([], Sum, Length) ->
   Sum / Length;
 average_acc([H | T], Sum, Length) ->
   average_acc(T, Sum + H, Length + 1).
+
+
+average_test() -> 2.0 = average([1,2,3]).
