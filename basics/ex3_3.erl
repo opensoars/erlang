07ab47d7649901a3598d02ca2af6b945e259800b
opensoars <sam_1700@hotmail.com>
2014-10-31 12:08:27 +0100

    Variable Final -> N, also small comment fix

--- basics/ex3_3.erl
+++ basics/ex3_3.erl
@@ -16,8 +16,8 @@
 -export([printInts/1, printEvenInts/1]).
 
 %%----------------------------------------------------------------------
-%% API function printInts, calls printInts_acc(I, Final)
-%% Prints integers from 1 to N
+%% API function printInts, calls printInts_acc(I, N)
+%% Prints integers from 1 to param N
 %% ex3_3:printInts(3).
 %% Number:1
 %% Number:2
@@ -26,26 +26,26 @@
 %%----------------------------------------------------------------------
 printInts(N) -> printInts_acc(1, N).
 
-printInts_acc(I, Final) when I =< Final ->
+printInts_acc(I, N) when I =< N ->
   io:format("Number:~p\n", [I]),
-  printInts_acc(I + 1, Final);
+  printInts_acc(I + 1, N);
 printInts_acc(_Iterator, _Final) ->
   ok.
 
 
 %%----------------------------------------------------------------------
-%% API function printEvenInts, calls printEvenInts_acc(I, Final)
-%% Prints even integers from 1 to N
+%% API function printEvenInts, calls printEvenInts_acc(I, N)
+%% Prints even integers from 1 to param N
 %% ex3_3:printEvenInts(5).
 %% Number:2
 %% Number:4
 %%----------------------------------------------------------------------
 printEvenInts(N) -> printEvenInts_acc(1, N).
 
-printEvenInts_acc(I, Final) when I =< Final, I rem 2 /= 0 ->
-  printEvenInts_acc(I + 1, Final);
-printEvenInts_acc(I, Final) when I =< Final, I rem 2 == 0 ->
+printEvenInts_acc(I, N) when I =< N, I rem 2 /= 0 ->
+  printEvenInts_acc(I + 1, N);
+printEvenInts_acc(I, N) when I =< N, I rem 2 == 0 ->
   io:format("Number:~p\n", [I]),
-  printEvenInts_acc(I + 1, Final);
+  printEvenInts_acc(I + 1, N);
 printEvenInts_acc(_I, _Final) ->
   ok.
