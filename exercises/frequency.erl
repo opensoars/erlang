7ddea7dae73216cfe3f59ddb571ce643ae4497c1
opensoars <sam_1700@hotmail.com>
2014-11-05 16:16:51 +0100

    Added headcom for frequency example/exercise

---
+++ exercises/frequency.erl
@@ -0,0 +1,19 @@
+%%%--------------------------------------------------------------------- 
+%%% Description module frequency
+%%%--------------------------------------------------------------------- 
+%%% Frequency allocating and deallocating example using 6 hardcoded
+%%% frequencies. This could be seen as a real phone call handling
+%%% mechanism.
+%%%
+%%%--------------------------------------------------------------------- 
+%%% Exports
+%%%--------------------------------------------------------------------- 
+%%% start()
+%%%   Starts our server so we are ready for allocations
+%%% allocate()
+%%%   Allocates an acual Frequency
+%%% deallocate(Frequency_to_deallocate)
+%%%   Deallocates Frequency_to_deallocate
+%%% stop()
+%%%   Stops our server
+%%%---------------------------------------------------------------------
